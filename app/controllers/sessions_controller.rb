a583d25af30d107971e3edfafb1f154b7f6d8b85
Ali Alsayed <a_alsayed87@hotmail.com>
2019-06-13 09:51:21 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:name])
-    if user && user.authenticate(params[:password])
+    if !user.empty? && user.authenticate(params[:password])
       redirect_to welcome_path
     else
       render :new
