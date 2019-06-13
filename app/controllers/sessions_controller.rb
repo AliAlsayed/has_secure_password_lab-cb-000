95a9ef3ca908b83ab661ae4ba03ca0da3bd490dc
Ali Alsayed <a_alsayed87@hotmail.com>
2019-06-13 10:24:11 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: "Ali ")#params[:name])
+    user = User.find_by(name: params[:name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to welcome_path
