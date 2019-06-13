dbf2ff02b85a105c11dca4870b60ea1743c8909b
Ali Alsayed <a_alsayed87@hotmail.com>
2019-06-13 09:53:24 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    fail
     user = User.find_by(name: params[:name])
     if user && user.try(:authenticate, params[:password])
       redirect_to welcome_path
